ce8a93c1a6f66bb7f75244f92166bbc40bfaed8f
unknown <ryan.sk.1717@gmail.com>
2019-03-18 21:35:02 -0400

    Change to address Chromes new restriction on cross domain requests

--- Client/script/background.js
+++ Client/script/background.js
@@ -5,6 +5,29 @@ chrome.runtime.onInstalled.addListener(function (details) {
 	}
 });
 
+//Chrome no longer allows content scripts to send cross domain requests.
+//Instead, make the call here and send back the response to the content script.
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == "request");
+  port.onMessage.addListener(function(msg) {
+		var reqPath = "?start=" + msg.start + "&end=" + msg.end + "&id=" + msg.id;
+		var req = $.ajax({
+					type : "GET",
+					url : "https://fuelcostmapextension.appspot.com/api/cost"
+							+ reqPath,
+					dataType : 'json'
+				});
+
+		var success = function(resp) {
+			port.postMessage({isSuccess:true, resp:resp});
+		}
+		var failure = function(resp) {
+			port.postMessage({isSuccess:false, resp:resp});
+		}
+		req.then(success, failure);
+  });
+});
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "openSettings")
